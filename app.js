0ba0ac2f1dda006c4b8583d1795614e6fc6e95f1
luljn <mbohlulajonathan4@gmail.com>
2026-01-21 23:17:50 +0100

    Basic Middlewares added

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
+//
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next(); // Pour terminer l'exécution du middleware et passer à l'éxécution du suivant.
+});
+
+//
+app.use((req, res, next) => {
+    res.status(201);
+});
+
+//
+app.use((req, res, next) => {
+    res.json({message: 'Votre requête a bien été reçue !'});
+    next();
+});
+
+//
 app.use((req, res) => {
-    res.json({message: 'Votre requête a bien été reçue !'})
+    console.log('Réponse envoyée avec succès !');
 });
 
 module.exports = app;
