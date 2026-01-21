51afc9f23aabfce770cc3a26ff51e49fa1dcd680
luljn <mbohlulajonathan4@gmail.com>
2026-01-21 23:44:25 +0100

    Port normalization, error handlind and logging added to the server

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
 
-//
+// Middleware 1 : enregistre "Requête reçue" dans la console.
 app.use((req, res, next) => {
     console.log('Requête reçue !');
     next(); // Pour terminer l'exécution du middleware et passer à l'éxécution du suivant.
 });
 
-//
+// Middleware 2 : ajoute un code d'état 201 à la réponse.
 app.use((req, res, next) => {
     res.status(201);
+    next();
 });
 
-//
+// Middleware 3 : envoie la réponse JSON.
 app.use((req, res, next) => {
     res.json({message: 'Votre requête a bien été reçue !'});
     next();
 });
 
-//
+// Middleware 4 : enregiste "Réponse envoyée avec succès !" dans la console.
 app.use((req, res) => {
     console.log('Réponse envoyée avec succès !');
 });
