fad35d15f0720d5d62d3958c096526e65e853637
luljn <mbohlulajonathan4@gmail.com>
2026-01-22 00:49:58 +0100

    cross-origin requests permitted

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Middleware pour résoudre les érreurs de CORS.
+// Middleware général(sera appliqué à toutes les requêtes envoyées au serveur).
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permet d'accéder à l'API depuis n'importe qu'elle origine. 
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Ajoute les headers mentionnés aux requêtes envoyées vers l'API.
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Permet d'envoyer des requêtes avec les méthodes mentionnées.
+    next();
+});
+
 // 1er argument : url visé par l'application (endpoint).
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
