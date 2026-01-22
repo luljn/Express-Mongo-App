2b28b204b9c805d96270be225789f26e1853650e
luljn <mbohlulajonathan4@gmail.com>
2026-01-22 18:55:44 +0100

    Endpoint to get a specific object added

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({error})); 
 });
 
+// Le ':' spécifie que la partie de la route le contenant est dynamique.
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
 // On intercepte les requêtes 'get' avec ce middleware.
 // 1er argument : url visé par l'application (endpoint).
 app.get('/api/stuff', (req, res, next) => {
