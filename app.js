35d0bc6e52e416c37cd0f8d5392c68498b22d277
luljn <mbohlulajonathan4@gmail.com>
2026-01-22 18:43:28 +0100

    Get endpoint updated

--- app.js
+++ app.js
@@ -34,25 +34,9 @@ app.post('/api/stuff', (req, res, next) => {
 // On intercepte les requêtes 'get' avec ce middleware.
 // 1er argument : url visé par l'application (endpoint).
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900, // en centimes.
-            userId: 'qsomihvqios', 
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900, // en centimes.
-            userId: 'qsomihvqios', 
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
