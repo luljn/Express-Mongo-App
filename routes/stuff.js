cef523daddf475739ede4cc22b65ba6775d96a10
luljn <mbohlulajonathan4@gmail.com>
2026-01-24 19:41:24 +0100

    jwt token auth implemented

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const stuffController = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
 // On intercepte les requêtes 'post' avec ce middleware.
-router.post('/', stuffController.createThing); // On n'appelle pas la fontion createThing,
-                                               // On l'applique à la route.
+router.post('/', auth, stuffController.createThing); // On n'appelle pas la fontion createThing,
+                                               // On l'applique à la route,
+                                               // On ajoute le middleware d'authentification avant le gestionnaire de routes.
 
 //On intercepte les requêtes 'put' pour un objet en particulier.
-router.put('/:id', stuffController.modifyThing);
+router.put('/:id', auth, stuffController.modifyThing);
 
 //On intercepte les requêtes 'delete' pour un objet en particulier.
-router.delete('/:id', stuffController.deleteThing);
+router.delete('/:id', auth, stuffController.deleteThing);
 
 // On intercepte les requêtes get pour un objet en particulier.
 // Le ':' spécifie que la partie de la route le contenant est dynamique.
-router.get('/:id', stuffController.getThingById);
+router.get('/:id', auth, stuffController.getThingById);
 
 // On intercepte les requêtes 'get' avec ce middleware.
 // 1er argument : url visé par l'application (endpoint).
-router.get('/', stuffController.getAllThings);
+router.get('/', auth, stuffController.getAllThings);
 
 module.exports = router;
