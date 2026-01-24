fb0cc5b2374682f3cf3e6a2b0c46933cfaa071cd
luljn <mbohlulajonathan4@gmail.com>
2026-01-24 19:50:28 +0100

    Code refactored

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try{
-        const token = req.headers.authorization.split('')[1];   // Extraction du token.
+        const token = req.headers.authorization.split(' ')[1];   // Extraction du token.
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');  // Décodage du token.
         const userId = decodedToken.userId;  // Extraction de l'Id utilisateur du token.
         req.auth = {
             userId: userId
         }; // Ajout du token à l'objet request, afin que les routes puissent l'utiliser.
+        next();
     }
     catch(error){
         res.status(401).json({ error });
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const stuffController = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const stuffController = require('../controllers/stuff');
+
+// On intercepte les requêtes 'get' avec ce middleware.
+// 1er argument : url visé par l'application (endpoint).
+router.get('/', auth, stuffController.getAllThings);
 
 // On intercepte les requêtes 'post' avec ce middleware.
 router.post('/', auth, stuffController.createThing); // On n'appelle pas la fontion createThing,
                                                // On l'applique à la route,
                                                // On ajoute le middleware d'authentification avant le gestionnaire de routes.
 
+// On intercepte les requêtes get pour un objet en particulier.
+// Le ':' spécifie que la partie de la route le contenant est dynamique.
+router.get('/:id', auth, stuffController.getThingById);
+
 //On intercepte les requêtes 'put' pour un objet en particulier.
 router.put('/:id', auth, stuffController.modifyThing);
 
 //On intercepte les requêtes 'delete' pour un objet en particulier.
 router.delete('/:id', auth, stuffController.deleteThing);
-
-// On intercepte les requêtes get pour un objet en particulier.
-// Le ':' spécifie que la partie de la route le contenant est dynamique.
-router.get('/:id', auth, stuffController.getThingById);
-
-// On intercepte les requêtes 'get' avec ce middleware.
-// 1er argument : url visé par l'application (endpoint).
-router.get('/', auth, stuffController.getAllThings);
 
 module.exports = router;
